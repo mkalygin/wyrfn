220b38bfc9e00c9079b858d1e7298fec5e307392
Michael Kalygin <michael.kalygin@gmail.com>
2016-12-26 01:59:32 +0300

    Implement fetching data from GitHub

--- server.js
+++ server.js
@@ -1,33 +1,86 @@
 var fs      = require('fs');
 var express = require('express');
+var GitHub  = require('github-api');
 
-var DEFAULT_PORT    = 1337;
-var PUBLIC_DIR_PATH = __dirname + '/public';
-var DATA_JSON_PATH  = __dirname + '/data.json';
+var DEFAULT_PORT      = 1337;
+var PUBLIC_DIR_PATH   = __dirname + '/public';
+var DATA_JSON_PATH    = __dirname + '/data.json';
+var GITHUB_TOKEN_PATH = __dirname + '/.token';
+var GITHUB_TOKEN      = getGitHubToken(GITHUB_TOKEN_PATH);
+var NUM_SINCE_DAYS    = 21;
+
+// Gets GitHub token from special token file.
+function getGitHubToken(path) {
+  return fs.readFileSync(path, 'utf8').trim();
+}
+
+// Fetches number of contributions to repos by these users from GitHub API.
+function getGitHubData(usersData, next) {
+  var gh = new GitHub({ token: GITHUB_TOKEN });
+
+  // Compute one week ago date to request commits from.
+  var since = new Date();
+  since.setDate(since.getDate() - NUM_SINCE_DAYS);
+
+  // Get names array for easier access.
+  var names = usersData.map(u => u.name);
+
+  var userPromises = usersData.map(u => {
+    var repoPromises = u.repos.map(r => {
+      var rp     = r.split('/');
+      var user   = rp[0];
+      var repo   = rp[1];
+      var ghRepo = gh.getRepo(user, repo);
+
+      return ghRepo.listCommits({
+        author: u.name,
+        since:  since
+      });
+    });
+
+    return Promise.all(repoPromises)
+      .then(lists => {
+        return lists.reduce((score, list) => score + list.data.length, 0);
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
+  return Promise.all(userPromises)
+    .then(scores => {
+      return scores.map((score, i) => ({
+        name:  names[i],
+        score: score
+      }));
+    })
+    .catch(err => {
+      next(err);
+    });
+}
 
 var app = express();
 
 app.use(express.static(PUBLIC_DIR_PATH));
 
 // Data provider for client's visualization.
-app.get('/data', function (req, res, next) {
-  fs.readFile(DATA_JSON_PATH, 'utf8', function (err, json) {
+app.get('/data', (req, res, next) => {
+  fs.readFile(DATA_JSON_PATH, 'utf8', (err, json) => {
     if (err) next(err);
 
+    // Parse JSON data with users and repos to be scanned from GitHub.
     var usersData = JSON.parse(json);
 
-    res.json([
-      { name: 'one', score: 1 },
-      { name: 'two', score: 3 },
-      { name: 'three', score: 7 },
-      { name: 'four', score: 4 },
-      { name: 'five', score: 9 }
-    ]);
+    // Get contributions data.
+    getGitHubData(usersData, next)
+      .then(data => {
+        res.json(data);
+      });
   });
 });
 
 // Global error handler.
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send(err.message);
 });
